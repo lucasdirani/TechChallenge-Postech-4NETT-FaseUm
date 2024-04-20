8723671ffa5ed9c4259f9ea40d3a81d3382fd452
Lucas Dirani <lruizd@protonmail.com>
2024-04-20 11:44:22 -0300

    feat(tests): adicionando testes unitários para o construtor da classe RegionValueObject.

--- src/Postech.PhaseOne.GroupEight.TechChallenge.Domain/ValueObjects/PhoneValueObject.cs
+++ src/Postech.PhaseOne.GroupEight.TechChallenge.Domain/ValueObjects/PhoneValueObject.cs
@@ -4,8 +4,6 @@ namespace Postech.PhaseOne.GroupEight.TechChallenge.Domain.ValueObjects
 {
     public record PhoneValueObject
     {
-        public AreaCodeValueObject? AreaCode { get; init; }
-
         [Required(ErrorMessage = "O número de telefone é obrigatório")]
         [StringLength(11)]
         public string? Number { get; init; }
