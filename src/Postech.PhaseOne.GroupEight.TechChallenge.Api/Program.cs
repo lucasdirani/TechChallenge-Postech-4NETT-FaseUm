cf11f9950928fb7823752cafe3f7423b17e3db05
rfulgencio3 <rfulgencio3@gmail.com>
2024-04-27 22:56:39 -0300

    Remoção de comentários de rota em Program.cs

--- src/Postech.PhaseOne.GroupEight.TechChallenge.Api/Program.cs
+++ src/Postech.PhaseOne.GroupEight.TechChallenge.Api/Program.cs
@@ -75,20 +75,5 @@ app.MapPost("/contacts", async (IMediator mediator, ContactInput request) =>
 }).WithName("SaveContacts")
 .WithOpenApi();
 
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast =  Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast")
-//.WithOpenApi();
-
 app.Run();
 
