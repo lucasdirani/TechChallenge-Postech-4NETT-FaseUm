e5b2379b228faf7b80a3524eec9c23f74f53354f
Lucas Dirani <lruizd@protonmail.com>
2024-06-30 15:14:37 -0300

    feat(observabilidade): adicionando as configurações iniciais necessárias para a observabilidade da aplicação com Prometheus e Grafana.

---
+++ src/Postech.PhaseOne.GroupEight.TechChallenge.Api/Middlewares/PrometheusMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Prometheus;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Postech.PhaseOne.GroupEight.TechChallenge.Api.Middlewares
+{
+    [ExcludeFromCodeCoverage]
+    internal static class PrometheusMiddleware
+    {
+        public static IApplicationBuilder UsePrometheusMiddleware(
+            this WebApplication app,
+            Counter endpointRequestCounterMetric,
+            Summary endpointRequestDurationMetric)
+        {
+            return app.Use(async (context, next) =>
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                endpointRequestCounterMetric.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                await next();
+                stopwatch.Stop();
+                endpointRequestDurationMetric.WithLabels(context.Request.Method, context.Request.Path).Observe(stopwatch.Elapsed.TotalSeconds);
+            });
+        }
+    }
+}
